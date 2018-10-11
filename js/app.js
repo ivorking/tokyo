a7e7ecd1b61ce9c4ecafd5d77d20a60bf5c2a70f
ivorking@gmail.com <ivorking@gmail.com>
2018-10-11 18:12:31 +0100

    update misc changes

--- js/app.js
+++ js/app.js
@@ -8,30 +8,46 @@
 // );
 
 const Highlight = props => (
-   <span className={`relative highlight highlight-blue`}>
+   <span className={`relative highlight highlight-${props.color}`}>
       <span className="relative z-2">{props.children}</span>
    </span>
-)
+);
+
+const Nav = () => (
+   <nav className = 'pt3 pt4-ns mb4 mb0-ns'>
+      <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0'>
+      {menu.map(item => (
+         <li className={`mh2-ns f6 f4-l tc ${item.className}`}>
+            <a className='white no-underline' href={item.href}>
+               {item.children}
+            </a>
+         </li>
+      ))}
+      </ul>
+   </nav>
+);
 
 const Intro = () => (
    <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
       <div className="mb3 mb4-ns">
+         <div>
          <Highlight color="aqua">Lost in Tokyo</Highlight>
-         is a directory of fun places to see, play in and explore, in Tokyo, Japan.
+         is a directory of fun places to see, play in and <Highlight color="yellow">explore</Highlight>, in <Highlight color="blue">Tokyo</Highlight>, Japan.
          </div>
          <div>
-         From museums and galleries, to Robot Restaurants and kitten cafes, Tokyo is the gift that keeps on giving. Dattebayo!
+         From <Highlight color="yellow">museums</Highlight> and galleries, to Robot Restaurants and kitten cafes, Tokyo is the gift that keeps on giving. Dattebayo!
          </div>
+      </div>
    </div>
 );
 
 const App = () => (
    <div>
       <div className="min-vh-100 ph4 flex flex-column">
+         <Nav />
          <Intro />
       </div>
       <div className="flex flex-wrap container">
-
       </div>
    </div>
 );
